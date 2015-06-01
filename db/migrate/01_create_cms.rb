85b60340bd55b2d9023e40ffb0a7b2f382245f39
Janusz Mordarski <janusz.mordarski@montrosesoftware.com>
2015-06-01 15:30:33 +0200

    Add redirect_to_path to CMS pages - updates d7975847
    This allows us to link_to path from app routes

--- db/migrate/01_create_cms.rb
+++ db/migrate/01_create_cms.rb
@@ -40,6 +40,7 @@ class CreateCms < ActiveRecord::Migration
       t.string  :label,           :null => false
       t.string  :slug
       t.string  :full_path,       :null => false
+      t.string  :redirect_to_path, :null => false
       t.text    :content_cache,   :limit => 16777215
       t.integer :position,        :null => false, :default => 0
       t.integer :children_count,  :null => false, :default => 0
